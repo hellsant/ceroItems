0878baed4fc1db763dc1802eabc94fc83f947dc4
douglas <douglassuareza@gmail.com>
2020-01-15 23:58:06 -0400

    add class

---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class User
+{
+    public string mane { get; set; }
+    public string lastName { get; set; }
+    public string password { get; set; }
+
+    public User()
+	{
+	}
+}
