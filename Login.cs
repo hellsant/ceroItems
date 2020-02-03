c1e84e62a526ed7941a2dd6e8fc9265442954d64
douglas <douglassuareza@gmail.com>
2020-02-03 19:19:21 -0400

    implemntacion del control de items

---
+++ Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TO-DO: Check login username & Password
+            SqlConnection connection = Connection.GetConnection();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(@"SELECT *
+                FROM [Stock].[dbo].[Login] Where UserName='" + textBox1.Text + "' and Password='" + textBox2.Text + "'", connection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Insertar un usuario o constraseña valida..!", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_Click(sender, e);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ReportForm/ProductsReport.cs
@@ -0,0 +1,30 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Stock.ReportForm
+{
+    public partial class ProductsReport : Form
+    {
+        ReportDocument cryrpt = new ReportDocument();
+        public ProductsReport()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductsReport_Load(object sender, EventArgs e)
+        {
+            cryrpt.Load(@"F:\Stock-master\Stock-master\Stock\Stock\Reports\Product.rpt");
+            SqlConnection connection = Connection.GetConnection();
+            connection.Open();
+            DataSet dst = new DataSet();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * From [Products]", connection);
+            DataTable data = new DataTable();
+            sqlDataAdapter.Fill(data);
+            cryrpt.SetDataSource(data);
+            crystalReportViewer1.ReportSource = cryrpt;
+        }
+    }
+}
